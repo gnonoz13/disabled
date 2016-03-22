9604cfff0e2ece3ef18aacd10e74f45911235f0b
kingnono <gnonoz13@gmail.com>
2016-03-22 06:53:39 +0000

    first push

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+    def home
+        
+    end
+    
+    def news
+    end
+    
+    def community
+    end
+    
+    def shop
+    end
+    
+end
